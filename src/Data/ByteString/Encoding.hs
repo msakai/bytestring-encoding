f5bc1423d0b01d81823be5bc8548ed9570ef155b
Masahiro Sakai <masahiro.sakai@gmail.com>
2018-08-14 23:37:13 +0900

    re-export encoding names

--- src/Data/ByteString/Encoding.hs
+++ src/Data/ByteString/Encoding.hs
@@ -1,7 +1,24 @@
 module Data.ByteString.Encoding
-  ( encode
+  (
+  -- * Converter functions
+    encode
   , decode
+
+  -- * Unicode encodings
+  , TextEncoding
+  , latin1
+  , utf8
+  , utf8_bom
+  , utf16
+  , utf16le
+  , utf16be
+  , utf32
+  , utf32le
+  , utf32be
+  , localeEncoding
+  , char8
+  , mkTextEncoding
   ) where
 
 import Data.ByteString.Encoding.Internal
-
+import System.IO
--- test/test.hs
+++ test/test.hs
@@ -2,16 +2,16 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 
+import qualified Data.ByteString.Encoding as Enc
 import qualified Data.ByteString.Encoding.Internal as Enc
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TL
-import System.IO
 
 import Test.Tasty.QuickCheck
 import Test.Tasty.TH
 
-checkEncode :: TextEncoding -> (TL.Text -> BL.ByteString) -> Property
+checkEncode :: Enc.TextEncoding -> (TL.Text -> BL.ByteString) -> Property
 checkEncode enc f =
   forAll arbitrary $ \(s::String) ->
   forAll (choose (2,16)) $ \inBufSize ->
@@ -19,7 +19,7 @@ checkEncode enc f =
     let s' = TL.pack s
     in Enc.encodeWith enc inBufSize outBufSize s' == f s'
 
-checkRoundTrip :: TextEncoding -> Property
+checkRoundTrip :: Enc.TextEncoding -> Property
 checkRoundTrip enc = 
   forAll arbitrary $ \(s::String) ->
   forAll (choose (2,16)) $ \inBufSizeE ->
@@ -30,34 +30,34 @@ checkRoundTrip enc =
     in (Enc.decodeWith enc inBufSizeD outBufSizeD $ Enc.encodeWith enc inBufSizeE outBufSizeE $ s') == s'
 
 prop_encode_utf8 :: Property
-prop_encode_utf8 = checkEncode utf8 TL.encodeUtf8
+prop_encode_utf8 = checkEncode Enc.utf8 TL.encodeUtf8
 
 prop_encode_utf16le :: Property
-prop_encode_utf16le = checkEncode utf16le TL.encodeUtf16LE
+prop_encode_utf16le = checkEncode Enc.utf16le TL.encodeUtf16LE
 
 prop_encode_utf16be :: Property
-prop_encode_utf16be = checkEncode utf16be TL.encodeUtf16BE
+prop_encode_utf16be = checkEncode Enc.utf16be TL.encodeUtf16BE
 
 prop_encode_utf32le :: Property
-prop_encode_utf32le = checkEncode utf32le TL.encodeUtf32LE
+prop_encode_utf32le = checkEncode Enc.utf32le TL.encodeUtf32LE
 
 prop_encode_utf32be :: Property
-prop_encode_utf32be = checkEncode utf32be TL.encodeUtf32BE
+prop_encode_utf32be = checkEncode Enc.utf32be TL.encodeUtf32BE
 
 prop_encode_decode_utf8 :: Property
-prop_encode_decode_utf8 = checkRoundTrip utf8
+prop_encode_decode_utf8 = checkRoundTrip Enc.utf8
 
 prop_encode_decode_utf16le :: Property
-prop_encode_decode_utf16le = checkRoundTrip utf16le
+prop_encode_decode_utf16le = checkRoundTrip Enc.utf16le
 
 prop_encode_decode_utf16be :: Property
-prop_encode_decode_utf16be = checkRoundTrip utf16be
+prop_encode_decode_utf16be = checkRoundTrip Enc.utf16be
 
 prop_encode_decode_utf32le :: Property
-prop_encode_decode_utf32le = checkRoundTrip utf32le
+prop_encode_decode_utf32le = checkRoundTrip Enc.utf32le
 
 prop_encode_decode_utf32be :: Property
-prop_encode_decode_utf32be = checkRoundTrip utf32be
+prop_encode_decode_utf32be = checkRoundTrip Enc.utf32be
 
 -- ---------------------------------------------------------------------
 -- Test harness
