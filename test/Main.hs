e5ef07d8e0ee9c84b4f9f26d987878d0f8a50d9a
Masahiro Sakai <masahiro.sakai@gmail.com>
2018-08-15 18:24:42 +0900

    add a test case for CodePage encodings

--- test/Main.hs
+++ test/Main.hs
@@ -5,10 +5,11 @@ import qualified System.Info
 import Test.Tasty
 
 import Base
+import CodePage
 import Iconv
 
 main :: IO ()
 main = defaultMain $ testGroup "bytestring-encoding" $
-  [ baseTestGroup ] ++
+  [ baseTestGroup, codepageTestGroup ] ++
   [ iconvTestGroup | System.Info.os /= "mingw32" ]
 
